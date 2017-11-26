1222e2eb2b517e2deda6df3abc5c4dd2b4238b4b
Mayur Dhagiya <mayurdhagiya@gmail.com>
2017-11-26 12:47:53 +0530

    Code add

---
+++ shutterstock.py
@@ -0,0 +1,23 @@
+# mayurdhagiya
+
+import sys, urllib2, re
+from BeautifulSoup import BeautifulSoup
+
+f = open("links.txt", 'w')
+
+search_word = raw_input("Enter the search word: ")
+url = "https://www.shutterstock.com/search?search_source=base_landing_page&language=en&searchterm="+ search_word +"&image_type=all"
+text = urllib2.urlopen(url).read()
+soup = BeautifulSoup(text)
+
+data = soup.findAll('div',attrs={'class':'overlay'})
+for div in data:
+    links = div.findAll('a')
+    for a in links:
+    	links = a["href"]
+    	links = str(links)
+    	if(links.startswith("/image-")):
+        	# print "https://www.shutterstock.com" + a['href'] + "\n"
+        	f.write("https://www.shutterstock.com" + a['href'] + "\n")
+
+f.close()
